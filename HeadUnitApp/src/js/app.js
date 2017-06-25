e3f781f296f9af0b21dd340710ed22332068433f
Johnny Opcode <johnnyopcode@gmail.com>
2017-06-25 11:03:45 -0400

    Emu WS Working

--- HeadUnitApp/src/js/app.js
+++ HeadUnitApp/src/js/app.js
@@ -1,1 +1,23 @@
-// Your code goes here
+/*
+  ImGM Head Unit Application
+  GMHackAI June 2017
+
+  (c) 2017 John Kost (johnnyopcode@gmail.com)
+ */
+
+// AI Smart Camera device discovery (tbd)
+var AI = "ws://localhost:9966";
+
+console.log("Connecting to AI");
+
+// Websocket connection handler
+var ws = new WebSocket(AI);
+    ws.onmessage = function(msg) {
+        console.log(msg.data);
+
+        setTimeout(function()
+        {
+            // keep the message flow going 
+            ws.send("OK")
+        }, 1000);
+    };
--- Zynq-Emu/camera-emu.js
+++ Zynq-Emu/camera-emu.js
@@ -2,12 +2,12 @@
   ImGM Zynq-Emu camera websocket emulation
   GMHackAI June 2017
 
-  John Kost (johnnyopcode@gmail.com)
+  (c) 2017 John Kost (johnnyopcode@gmail.com)
  */
 
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 6666 });
+const wss = new WebSocket.Server({ port: 9966 });
 
 console.log('Zynq Camera Emulation Running..');
 
@@ -16,5 +16,22 @@ wss.on('connection', function connection(ws) {
     console.log('received: %s', message);
   });
 
-  ws.send('something');
+  ws.send(JSON.stringify(genAIresponse()));
 });
+
+function genAIresponse() {
+    // We model the AI output as a JSON object for easy consumption by the Head Unit
+    var ojson = {"Awareness":100, "Engagement":100, "UpsetEvt":false};
+
+    // Add some randomness to help the animation in the UI
+    ojson.Awareness = 40 + 60 * Math.random();
+    ojson.Engagement = 40 + 60 * Math.random();
+
+    if (Math.round(Math.random()) > 0){
+      ojson.UpsetEvt = true;
+    } else {
+      ojson.UpsetEvt = false;
+    }
+
+  return ojson;
+}
