dbfd9c4622053154e66c0831cfd839a74ab10c7c
Johnny Opcode <johnnyopcode@gmail.com>
2017-06-26 00:27:19 -0400

    Sleepy Head wants to go to bed..
    Tensorflow work ahead!

--- HeadUnitApp/src/js/app.js
+++ HeadUnitApp/src/js/app.js
@@ -3,17 +3,31 @@
   GMHackAI June 2017
 
   (c) 2017 John Kost (johnnyopcode@gmail.com)
- */
+*/
+
+var Awareness;
+var Engagement;
+var UpsetEvt;
+var EventCount = 0;
+
+var AlertAwareness = false;
 
 // AI Smart Camera device discovery (tbd)
 var AI = "ws://localhost:9966";
 
 console.log("Connecting to AI");
+var id = gm.voice.startTTS(success, "The future is here today. Drive safe.");
+
+function success() {
+  // let it roll
+}
 
 // Websocket connection handler
 var ws = new WebSocket(AI);
     ws.onmessage = function(msg) {
         console.log(msg.data);
+
+        deserializedata(msg.data)
 
         setTimeout(function()
         {
@@ -21,3 +35,41 @@ var ws = new WebSocket(AI);
             ws.send("OK")
         }, 1000);
     };
+
+function deserializedata(data) {
+    var AIobject = JSON.parse(data);
+
+    // the AIobject can contain a multitude of data, some of which we ignore
+    Awareness = Math.ceil(AIobject.Awareness);
+    Engagement = Math.ceil(AIobject.Engagement);
+    UpsetEvt = AIobject.UpsetEvt;
+    if (UpsetEvt) {
+        EventCount++;
+    }
+
+    updatescreen();
+    announcement();
+}
+
+function updatescreen() {
+    document.getElementById("Awareness").innerHTML = "Awareness: " + Awareness; 
+    document.getElementById("Engagement").innerHTML = "Engagement: " + Engagement;
+    document.getElementById("UpsetEvt").innerHTML = "UpsetEvt: " + UpsetEvt;
+
+    TagCanvas.Update("myCanvas");
+}
+
+function announcement() {
+    if (Awareness < 45 && !AlertAwareness) {
+        var id = gm.voice.startTTS(okfine, "Please focus on driving. The life you save may be your own.");
+
+        function okfine() {
+            AlertAwareness = true;
+        }
+    }
+
+    // deadband around awareness reset
+    if (Awareness > 95) {
+        AlertAwareness = false;
+    }
+}
--- Zynq-Emu/camera-emu.js
+++ Zynq-Emu/camera-emu.js
@@ -3,17 +3,19 @@
   GMHackAI June 2017
 
   (c) 2017 John Kost (johnnyopcode@gmail.com)
- */
+*/
 
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 9966 });
 
-console.log('Zynq Camera Emulation Running..');
+console.log("Zynq Smart Camera Emulation Running..");
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+    
+    ws.send(JSON.stringify(genAIresponse()));
   });
 
   ws.send(JSON.stringify(genAIresponse()));
@@ -21,13 +23,13 @@ wss.on('connection', function connection(ws) {
 
 function genAIresponse() {
     // We model the AI output as a JSON object for easy consumption by the Head Unit
-    var ojson = {"Awareness":100, "Engagement":100, "UpsetEvt":false};
+    var ojson = {"Firmware":"0.1.0", "Awareness":100, "Engagement":100, "UpsetEvt":false};
 
     // Add some randomness to help the animation in the UI
     ojson.Awareness = 40 + 60 * Math.random();
     ojson.Engagement = 40 + 60 * Math.random();
 
-    if (Math.round(Math.random()) > 0){
+    if (Math.round(Math.random()) > 0) {
       ojson.UpsetEvt = true;
     } else {
       ojson.UpsetEvt = false;
